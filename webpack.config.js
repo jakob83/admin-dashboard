7ce8ec063331055cb8762b8f17ecc8390916d4c7
jakob83 <jakobpllo@gmail.com>
2024-05-05 20:23:17 +0200

    created a rough template

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { title } = require('process');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'admin dashboard',
+            inject: 'body',
+            template: './src/index.html',
+        }
+        )
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+
+        ],
+    },
+}
